067e159e9e7e318baf08eb4ba7161cb327a5aca5
Sebastian Marines <hi@sebastianmarines.com>
2025-04-02 20:42:46 -0600

    Format code

--- iam.tf
+++ iam.tf
@@ -59,28 +59,28 @@ resource "aws_iam_role_policy_attachment" "attach-cdk" {
   policy_arn = aws_iam_policy.cdk.arn
 }
 resource "aws_iam_role_policy_attachment" "appsync_admin_attachment" {
-  role       =  aws_iam_role.this.name
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AWSAppSyncAdministrator"
 }
 
 resource "aws_iam_role_policy_attachment" "cloudformation_full_access_attachment" {
-  role       =  aws_iam_role.this.name
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCloudFormationFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "Lambda_Full_Access_attachment" {
-  role       =  aws_iam_role.this.name
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonS3FullAccess_attachment" {
-  role       =  aws_iam_role.this.name
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 resource "aws_iam_policy" "dynamodb_full_access" {
   name        = "dynamodb-full-access-policy"
   description = "Policy for full access to DynamoDB"
-  policy      = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
--- route53.tf
+++ route53.tf
@@ -12,10 +12,10 @@ data "cloudflare_zone" "main_zone" {
 
 resource "cloudflare_dns_record" "ns" {
   for_each = toset(aws_route53_zone.dev.name_servers) # Convierte la lista en un conjunto para iterar
-  zone_id = data.cloudflare_zone.main_zone.zone_id
-  name    = "app.awscommunity.mx" # Nombre de la zona en Cloudflare
-  type    = "NS"
-  ttl     = 1 
-  content = each.value
+  zone_id  = data.cloudflare_zone.main_zone.zone_id
+  name     = "app.awscommunity.mx" # Nombre de la zona en Cloudflare
+  type     = "NS"
+  ttl      = 1
+  content  = each.value
 }
 
